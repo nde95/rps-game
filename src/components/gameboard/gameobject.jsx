c020f6b3c26637296a2da33a1146f5dbf5485bc3
nde95 <ndennison268@gmail.com>
2023-11-20 15:01:07 -0500

    adding icons and hiding ID number

--- src/components/gameboard/gameobject.jsx
+++ src/components/gameboard/gameobject.jsx
@@ -2,7 +2,7 @@ const GameObject = ({ id, left, top }) => {
     const className = `game-object object-${id}`;
     return (
         <div className={className} style={{ left, top }}>
-            {id}
+            {/* {id} */}
         </div>
     );
 };
